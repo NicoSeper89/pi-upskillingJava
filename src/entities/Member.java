68f102d11647003b1585d8a136d46b2c89afad07
nicoseper89 <nicosepertino@hotmail.com>
2023-08-22 01:06:30 -0300

    Fix Member class.

--- src/entities/Member.java
+++ src/entities/Member.java
@@ -1,6 +1,5 @@
 package entities;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Member {
@@ -8,25 +7,25 @@ public class Member {
     private Integer id;
     private String name;
     private String surname;
-    private Character category;
+    private String category;
     private String address;
     private String phone;
     private String email;
-    private List<Fee> fees;
 
-    public Member(Integer id, String name, String surname, String address, String phone, String email, List<Fee> fees) {
+    public Member(Integer id, String name, String surname, String category, String address, String phone, String email) {
         this.id = id;
         this.name = name;
         this.surname = surname;
+        this.category = category;
         this.address = address;
         this.phone = phone;
         this.email = email;
-        this.fees = fees;
     }
 
-    public Member(String name, String surname, String address, String phone, String email) {
+    public Member(String name, String surname, String category, String address, String phone, String email) {
         this.name = name;
         this.surname = surname;
+        this.category = category;
         this.address = address;
         this.phone = phone;
         this.email = email;
@@ -52,11 +51,11 @@ public class Member {
         this.surname = surname;
     }
 
-    public Character getCategory() {
+    public String getCategory() {
         return category;
     }
 
-    public void setCategory(Character category) {
+    public void setCategory(String category) {
         this.category = category;
     }
 
