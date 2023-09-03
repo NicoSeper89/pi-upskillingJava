f7767388f6f9c1a05f062c427a282040e7c93ab9
nicoseper89 <nicosepertino@hotmail.com>
2023-09-03 17:08:46 -0300

    Implement getAll_ShouldReturnListOfMembersDtos_WhenDataBaseHasData() to test getAll() method of MemberDAOImpl class.

--- test/main/dao/implH2/MemberDAOImplTest.java
+++ test/main/dao/implH2/MemberDAOImplTest.java
@@ -2,6 +2,7 @@ package main.dao.implH2;
 
 import main.dao.dto.MemberDTO;
 import main.dao.dto.PersonDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,7 +11,10 @@ import org.mockito.MockitoAnnotations;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -21,6 +25,8 @@ class MemberDAOImplTest {
     private Connection mockConnection;
     @Mock
     private PreparedStatement mockPreparedStatement;
+    @Mock
+    private ResultSet mockResultSet;
 
     @InjectMocks
     private MemberDAOImpl memberDao;
@@ -105,4 +111,51 @@ class MemberDAOImplTest {
         verify(mockPreparedStatement).executeUpdate();
     }
 
+    @Test
+    void getAll_ShouldReturnListOfMembersDtos_WhenDataBaseHasData() throws SQLException {
+        //GIVEN
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO(1,
+                "Juan",
+                "Gomez",
+                "A",
+                "Moreno 340",
+                "666-555-333",
+                ""));
+        memberList.add(new MemberDTO(2,
+                "Facundo",
+                "Rodriguez",
+                "B",
+                "Cordoba 2360",
+                "444-555-313",
+                "facurodriguez@hotmail.com"));
+
+        when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
+        when(mockResultSet.next()).thenReturn(true, true, false);
+        when(mockResultSet.getInt("id")).thenReturn(1, 2);
+        when(mockResultSet.getString("name")).thenReturn("Juan", "Facundo");
+        when(mockResultSet.getString("surname")).thenReturn("Gomez", "Rodriguez");
+        when(mockResultSet.getString("category")).thenReturn("A", "B");
+        when(mockResultSet.getString("address")).thenReturn("Moreno 340", "Cordoba 2360");
+        when(mockResultSet.getString("phone")).thenReturn("666-555-333", "444-555-313");
+        when(mockResultSet.getString("email")).thenReturn("", "facurodriguez@hotmail.com");
+
+        //WHEN
+        List<MemberDTO> resultList = memberDao.getAll();
+
+        //THEN
+        verify(mockPreparedStatement).executeQuery();
+        verify(mockResultSet, times(2)).getInt("id");
+        verify(mockResultSet, times(2)).getString("name");
+        verify(mockResultSet, times(2)).getString("surname");
+        verify(mockResultSet, times(2)).getString("category");
+        verify(mockResultSet, times(2)).getString("address");
+        verify(mockResultSet, times(2)).getString("phone");
+        verify(mockResultSet, times(2)).getString("email");
+
+        //Assertions
+        Assertions.assertEquals(memberList.size(), resultList.size());
+        Assertions.assertEquals(memberList.get(0).getId(), resultList.get(0).getId());
+        Assertions.assertEquals(memberList.get(1).getId(), resultList.get(1).getId());
+    }
 }
