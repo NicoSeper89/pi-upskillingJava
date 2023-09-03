5ba693c06db55396b9c9c018b2bee9794c76cc52
nicoseper89 <nicosepertino@hotmail.com>
2023-09-03 19:39:21 -0300

    Implement getAll_ShouldReturnListOfFeesDtos_WhenDatabaseHasData() to test getAll() method of FeeDAOImpl class.

--- test/main/dao/implH2/FeeDAOImplTest.java
+++ test/main/dao/implH2/FeeDAOImplTest.java
@@ -11,6 +11,7 @@ import org.mockito.MockitoAnnotations;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -27,6 +28,8 @@ class FeeDAOImplTest {
     private Connection mockConnection;
     @Mock
     private PreparedStatement mockPreparedStatement;
+    @Mock
+    private ResultSet mockResultSet;
     @InjectMocks
     private FeeDAOImpl feeDao;
 
@@ -95,5 +98,47 @@ class FeeDAOImplTest {
         // THEN
         verify(mockPreparedStatement).setInt(1, feeId);
         verify(mockPreparedStatement).executeUpdate();
+    }
+
+    @Test
+    void getAll_ShouldReturnListOfFeesDtos_WhenDatabaseHasData() throws SQLException {
+        // GIVEN
+        List<FeeDTO> feeList = new ArrayList<>();
+
+        feeList.add(new FeeDTO(1,
+                4000,
+                "09-2023",
+                false,
+                new MemberDTO(1)));
+
+        feeList.add(new FeeDTO(2,
+                2000,
+                "09-2023",
+                true,
+                new MemberDTO(2)));
+
+        when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
+        when(mockResultSet.next()).thenReturn(true, true, false);
+        when(mockResultSet.getInt("id")).thenReturn(1, 2);
+        when(mockResultSet.getInt("amount")).thenReturn(4000, 2000);
+        when(mockResultSet.getString("generated_date")).thenReturn("09-2023", "09-2023");
+        when(mockResultSet.getBoolean("paid")).thenReturn(false, true);
+        when(mockResultSet.getInt("owner_member_id")).thenReturn(1, 2);
+
+        // WHEN
+        List<FeeDTO> resultList = feeDao.getAll();
+
+        // THEN
+        verify(mockPreparedStatement).executeQuery();
+        verify(mockResultSet, times(2)).getInt("id");
+        verify(mockResultSet, times(2)).getInt("amount");
+        verify(mockResultSet, times(2)).getString("generated_date");
+        verify(mockResultSet, times(2)).getBoolean("paid");
+        verify(mockResultSet, times(2)).getInt("owner_member_id");
+
+        // Assertions
+        Assertions.assertEquals(feeList.size(), resultList.size());
+        Assertions.assertEquals(feeList.get(0).toString(), resultList.get(0).toString());
+        Assertions.assertEquals(feeList.get(1).toString(), resultList.get(1).toString());
     }
 }
