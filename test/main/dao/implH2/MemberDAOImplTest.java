a2ed5c219cf36f9150dcb08229aa28d906c6f702
nicoseper89 <nicosepertino@hotmail.com>
2023-09-03 15:07:46 -0300

    Define MemberDAOImplTest and implement insert_ShouldInsertMember_WhenValidMemberDto() to test insert of MemberDAOImpl class

---
+++ test/main/dao/implH2/MemberDAOImplTest.java
@@ -0,0 +1,63 @@
+package main.dao.implH2;
+
+import main.dao.dto.MemberDTO;
+import main.dao.dto.PersonDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class MemberDAOImplTest {
+
+    @Mock
+    private Connection mockConnection;
+    @Mock
+    private PreparedStatement mockPreparedStatement;
+
+    @InjectMocks
+    private MemberDAOImpl memberDao;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        MockitoAnnotations.initMocks(this);
+        when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
+    }
+
+    @Test
+    void insert_ShouldInsertMember_WhenValidMemberDto() throws SQLException {
+
+        // GIVEN
+        PersonDTO memberDTO = new MemberDTO(
+                "Juan",
+                "Gomez",
+                "A",
+                "Moreno 54",
+                "555-453-458",
+                "juangomez@hotmail.com"
+        );
+
+        when(mockPreparedStatement.executeUpdate()).thenReturn(1);
+
+        // WHEN
+        memberDao.insert(memberDTO);
+
+        // THEN
+        verify(mockPreparedStatement).setString(1, memberDTO.getName());
+        verify(mockPreparedStatement).setString(2, memberDTO.getSurname());
+        verify(mockPreparedStatement).setString(3, memberDTO.getCategory());
+        verify(mockPreparedStatement).setString(4, memberDTO.getAddress());
+        verify(mockPreparedStatement).setString(5, memberDTO.getPhone());
+        verify(mockPreparedStatement).setString(6, memberDTO.getEmail());
+        verify(mockPreparedStatement).executeUpdate();
+
+    }
+
+}
